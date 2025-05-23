a624dd7afa251e663dde729659281e4bda150ad3
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-05-23 16:39:20 +0500

    step1

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-make install
+export PATH="/opt/render/.local/bin:$PATH"
+
+uv venv
+uv pip install flask python-dotenv gunicorn
+
+# активируем виртуальное окружение — важно для render-start
+source .venv/bin/activate
