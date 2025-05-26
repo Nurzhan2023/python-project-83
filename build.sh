c5828997cca550338a9d69166f5ac7e63f54649d
Nurzhan2023 <nurzhan02061990@gmail.com>
2025-05-26 11:30:24 +0500

    step1

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Установка uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-make install
+
+# Добавляем uv в PATH
+export PATH="$HOME/.local/bin:$PATH"
+
+# Устанавливаем зависимости
+uv sync
